48112c3b3f3547297cd4a546edd7e5b04fef35de
BeenCoding <40044042+BeenCoding@users.noreply.github.com>
2020-08-16 21:06:58 +0300

    Update powers2.cpp

--- powers2/powers2.cpp
+++ powers2/powers2.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main(int argc, char **argv){
 	int i,bits,n,n_dupl,k,counterOne,inputs;
 	ifstream infile(argv[1]);
-	infile >> inputs;                            // read first line of txt to know how many lines we're reading	
+	infile >> inputs;                            				// read first line of txt to know how many lines we're reading	
 	
-	for(int line=0; line<inputs; line++)         // for loop for reading every line in txt file
+	for(int line=0; line<inputs; line++)         				// for loop for reading every line in txt file
 	{
 		//initializations.
 		infile >> n >> k;
@@ -19,7 +19,7 @@ int main(int argc, char **argv){
 		n_dupl = n;
 		
 		
-		for(i=0; n_dupl>0; i++)                    // for loop to make the number n in its binary form
+		for(i=0; n_dupl>0; i++)                    			// for loop to make the number n in its binary form
 		{    
 			binary[i] = n_dupl%2;    
 			n_dupl = n_dupl/2;
@@ -27,14 +27,14 @@ int main(int argc, char **argv){
 				counterOne++;
 		}
 		
-		if(k>n || k<counterOne) cout<<"[]"<<endl;   // if these statements are true, the certain couple(n,k) cant be solved
+		if(k>n || k<counterOne) cout<<"[]"<<endl;   			// if these statements are true, the certain couple(n,k) cant be solved
 		else{
-			while(counterOne != k){                   // while counterOne is not k then 
-				for(int i=1;i<bits;i++){                // we need to search how many elements in i position 
-					if(binary[i]!= 0){                    // can be represented as elements in i-1 position
-						binary[i]-=1;                       // for this reason we subtract one from i-th element and 
-						binary[i-1]+=2;                     // add 2 in i-1 element. 
-						counterOne++;                       // And add one in the counter that keeps track of how many 1 we used.
+			while(counterOne != k){                   		// while counterOne is not k then 
+				for(int i=1;i<bits;i++){                	// we need to search how many elements in i position 
+					if(binary[i]!= 0){                      // can be represented as elements in i-1 position
+						binary[i]-=1;                   // for this reason we subtract one from i-th element and 
+						binary[i-1]+=2;                 // add 2 in i-1 element. 
+						counterOne++;                   // And add one in the counter that keeps track of how many 1 we used.
 						break;
 					}
 				}
